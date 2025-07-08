9ad94e73f3982908e70325ad185441649246cead
dhd1956 <dhd1956@users.noreply.github.com>
2025-07-08 09:14:03 -0400

    Refactor: Cleaned up start blog content and renamed project folder

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Develop My Piano Style',
+  author: 'David Davies',
+  headerTitle: 'Develop My Piano Style',
+  description: 'This blog chronicles my musical journey playing piano as a serious hobby',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
